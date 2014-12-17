07f0f1b031c3818044d4008e626d2444bddfe197
Tung Vu <vutung.ct@gmail.com>
2014-12-18 01:02:28 +0700

    Adding more structure to CRM plugin

--- wp-content/plugins/crm/admin/admin.php
+++ wp-content/plugins/crm/admin/admin.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * Handles the admin setup and functions for the plugin.
+ *
+ * @package Members
+ * @subpackage Admin
+ */
+
+/* Set up the administration functionality. */
+add_action ( 'admin_menu', 'crm_admin_setup' );
+
+/**
+ * Sets up any functionality needed in the admin.
+ *
+ * @since 0.2.0
+ */
+function crm_admin_setup()
+{
+    
+    /* Create the Manage Roles page. */
+    add_menu_page ( esc_attr__ ( 'CRM', 'members' ), esc_attr__ ( 'CRM', 'members' ), 'read', 'crm', 'crm_list_customers' );
+}
+?>
