902f204dd00c5efd4f27faa5cda8fcec2c204f18
vutung2311 <vutung.ct@gmail.com>
2014-12-22 08:23:11 +0700

    Continuing on preparing structure for CRM plugin

--- wp-content/plugins/crm/admin/admin.php
+++ wp-content/plugins/crm/admin/admin.php
@@ -2,7 +2,7 @@
 /**
  * Handles the admin setup and functions for the plugin.
  *
- * @package Members
+ * @package CRM
  * @subpackage Admin
  */
 
@@ -18,6 +18,42 @@ function crm_admin_setup()
 {
     
     /* Create the Manage Roles page. */
-    add_menu_page ( esc_attr__ ( 'CRM', 'members' ), esc_attr__ ( 'CRM', 'members' ), 'read', 'crm', 'crm_list_customers' );
+    add_menu_page ( esc_attr__ ( 'CRM', 'members' ), esc_attr__ ( 'CRM', 'members' ), 'read', 'crm', 'crm_list_customers', '', '72.5' );
+}
+function crm_list_customers()
+{
+    $title = __('CRM');
+    $wp_list_table = _get_list_table('WP_Users_List_Table');
+    ?>
+<div class="wrap">
+	<h2>
+<?php
+    echo esc_html ( $title );
+    if (current_user_can ( 'create_users' )) {
+        ?>
+	<a href="user-new.php" class="add-new-h2"><?php echo esc_html_x( 'Add New', 'user' ); ?></a>
+<?php } elseif ( is_multisite() && current_user_can( 'promote_users' ) ) { ?>
+	<a href="user-new.php" class="add-new-h2"><?php echo esc_html_x( 'Add Existing', 'user' ); ?></a>
+<?php
+    
+}
+    
+    if ($usersearch)
+        printf ( '<span class="subtitle">' . __ ( 'Search results for &#8220;%s&#8221;' ) . '</span>', esc_html ( $usersearch ) );
+    ?>
+</h2>
+
+<?php $wp_list_table->views(); ?>
+
+<form action="" method="get">
+
+<?php $wp_list_table->search_box( __( 'Search Users' ), 'user' ); ?>
+
+<?php $wp_list_table->display(); ?>
+</form>
+
+	<br class="clear" />
+</div>
+<?php
 }
 ?>
--- wp-content/plugins/crm/crm.php
+++ wp-content/plugins/crm/crm.php
@@ -16,104 +16,126 @@
  */
 
 /**
+ *
  * @since 0.1
  */
-class Crm_Load {
-
-	/**
-	 * PHP5 constructor method.
-	 *
-	 * @since 0.2.0
-	 */
-	function __construct() {
-		global $crm;
-
-		/* Set up an empty class for the global $members object. */
-		$crm = new stdClass;
-
-		/* Set the constants needed by the plugin. */
-		add_action( 'plugins_loaded', array( &$this, 'constants' ), 1 );
-
-		/* Internationalize the text strings used. */
-		add_action( 'plugins_loaded', array( &$this, 'i18n' ), 2 );
-
-		/* Load the functions files. */
-		add_action( 'plugins_loaded', array( &$this, 'includes' ), 3 );
-
-		/* Load the admin files. */
-		add_action( 'plugins_loaded', array( &$this, 'admin' ), 4 );
-
-		/* Register activation hook. */
-		register_activation_hook( __FILE__, array( &$this, 'activation' ) );
-	}
-
-	/**
-	 * Defines constants used by the plugin.
-	 *
-	 * @since 0.2.0
-	 */
-	function constants() {
-
-		/* Set constant path to the members plugin directory. */
-		define( 'CRM_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
-
-		/* Set constant path to the members plugin URL. */
-		define( 'CRM_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
-
-		/* Set the constant path to the members includes directory. */
-		define( 'CRM_INCLUDES', CRM_DIR . trailingslashit( 'includes' ) );
-
-		/* Set the constant path to the members admin directory. */
-		define( 'CRM_ADMIN', CRM_DIR . trailingslashit( 'admin' ) );
-	}
-
-	/**
-	 * Loads the initial files needed by the plugin.
-	 *
-	 * @since 0.2.0
-	 */
-	function includes() {
-
-		/* Load the plugin functions file. */
-		require_once( CRM_INCLUDES . 'functions.php' );
-	}
-
-	/**
-	 * Loads the translation files.
-	 *
-	 * @since 0.2.0
-	 */
-	function i18n() {
-
-		/* Load the translation of the plugin. */
-		load_plugin_textdomain( 'crm', false, 'crm/languages' );
-	}
-
-	/**
-	 * Loads the admin functions and files.
-	 *
-	 * @since 0.2.0
-	 */
-	function admin() {
-
-		/* Only load files if in the WordPress admin. */
-		if ( is_admin() ) {
-
-			/* Load the main admin file. */
-			require_once( CRM_ADMIN . 'admin.php' );
-		}
-	}
-
-	/**
-	 * Method that runs only when the plugin is activated.
-	 *
-	 * @since 0.2.0
-	 */
-	function activation() {
-	    
-	}
+class Crm_Load
+{
+    
+    /**
+     * PHP5 constructor method.
+     *
+     * @since 0.2.0
+     */
+    function __construct()
+    {
+        global $crm;
+        
+        /* Set up an empty class for the global $members object. */
+        $crm = new stdClass ();
+        
+        /* Set the constants needed by the plugin. */
+        add_action ( 'plugins_loaded', array(
+            &$this,
+            'constants'
+        ), 1 );
+        
+        /* Internationalize the text strings used. */
+        add_action ( 'plugins_loaded', array(
+            &$this,
+            'i18n'
+        ), 2 );
+        
+        /* Load the functions files. */
+        add_action ( 'plugins_loaded', array(
+            &$this,
+            'includes'
+        ), 3 );
+        
+        /* Load the admin files. */
+        add_action ( 'plugins_loaded', array(
+            &$this,
+            'admin'
+        ), 4 );
+        
+        /* Register activation hook. */
+        register_activation_hook ( __FILE__, array(
+            &$this,
+            'activation'
+        ) );
+    }
+    
+    /**
+     * Defines constants used by the plugin.
+     *
+     * @since 0.2.0
+     */
+    function constants()
+    {
+        
+        /* Set constant path to the members plugin directory. */
+        define ( 'CRM_DIR', trailingslashit ( plugin_dir_path ( __FILE__ ) ) );
+        
+        /* Set constant path to the members plugin URL. */
+        define ( 'CRM_URI', trailingslashit ( plugin_dir_url ( __FILE__ ) ) );
+        
+        /* Set the constant path to the members includes directory. */
+        define ( 'CRM_INCLUDES', CRM_DIR . trailingslashit ( 'includes' ) );
+        
+        /* Set the constant path to the members admin directory. */
+        define ( 'CRM_ADMIN', CRM_DIR . trailingslashit ( 'admin' ) );
+    }
+    
+    /**
+     * Loads the initial files needed by the plugin.
+     *
+     * @since 0.2.0
+     */
+    function includes()
+    {
+        
+        /* Load the plugin functions file. */
+        require_once (CRM_INCLUDES . 'functions.php');
+    }
+    
+    /**
+     * Loads the translation files.
+     *
+     * @since 0.2.0
+     */
+    function i18n()
+    {
+        
+        /* Load the translation of the plugin. */
+        load_plugin_textdomain ( 'crm', false, 'crm/languages' );
+    }
+    
+    /**
+     * Loads the admin functions and files.
+     *
+     * @since 0.2.0
+     */
+    function admin()
+    {
+        
+        /* Only load files if in the WordPress admin. */
+        if (is_admin ()) {
+            
+            /* Load the main admin file. */
+            require_once (CRM_ADMIN . 'admin.php');
+        }
+    }
+    
+    /**
+     * Method that runs only when the plugin is activated.
+     *
+     * @since 0.2.0
+     */
+    function activation()
+    {
+    }
 }
 
-$crm_load = new Crm_Load();
+$crm_load = new Crm_Load ();
 
 ?>
