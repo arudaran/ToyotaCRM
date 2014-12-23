d3cee4c8176450653e762a7d6a777acdcbd2625b
Tung Vu <tung.vu@audiencemedia.com>
2014-12-23 12:13:09 +0700

    Construct CRM customer class

--- wp-content/plugins/crm/crm.php
+++ wp-content/plugins/crm/crm.php
@@ -4,11 +4,11 @@
  * Description: A plugin was meaned to be support CRM management inside WordPress Dashboard.
  * Version: 0.1
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
- * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
  * that you can use any other version of the GPL.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package WordPress_CRM
@@ -21,7 +21,7 @@
  */
 class WordPress_Crm_Load
 {
-    
+
     /**
      * PHP5 constructor method.
      *
@@ -30,53 +30,55 @@ class WordPress_Crm_Load
     function __construct()
     {
         global $wp_crm;
-        
+
         /* Set up an empty class for the global $members object. */
         $crm = new stdClass();
-        
+
         /* Set the constants needed by the plugin. */
         add_action('plugins_loaded', array(&$this,'constants'), 1);
-        
+
         /* Internationalize the text strings used. */
         add_action('plugins_loaded', array(&$this,'i18n'), 2);
-        
+
         /* Load the functions files. */
         add_action('plugins_loaded', array(&$this,'includes'), 3);
-        
+
         /* Load the admin files. */
         add_action('plugins_loaded', array(&$this,'admin'), 4);
-        
+
         /* Register customer type */
         add_action('init', array(&$this,'init'), 1);
-        
+
         /* Register activation hook. */
         register_activation_hook(__FILE__, array(&$this,'activation'));
-        
+
         /* Register uninstall hook */
-        register_uninstall_hook(__FILE__, array(&$this,'uninstall'));
+        register_deactivation_hook(__FILE__, array(&$this,'deactivation'));
     }
-    
+
     /**
      * Register customer post type
      */
     function init()
     {
-        $args = array(
-                'description' => 'Post type to store customer of CRM',
-                'public' => false,
-                'exclude_from_search' => true,
-                'publicly_queryable' => false,
-                'show_ui' => false,
-                'show_in_nav_menus' => false,
-                'show_in_menu' => false,
-                'show_in_admin_bar' => false,
-                'supports' => array(
-                        'author',
-                        'custom-fields'),
-                'query_var' => false);
+        $args = array (
+            'description' => 'Post type to store customer of CRM',
+            'public' => false,
+            'exclude_from_search' => true,
+            'publicly_queryable' => false,
+            'show_ui' => false,
+            'show_in_nav_menus' => false,
+            'show_in_menu' => false,
+            'show_in_admin_bar' => false,
+            'supports' => array (
+                'author',
+                'custom-fields'
+            ),
+            'query_var' => false
+        );
         register_post_type('customer', $args);
     }
-    
+
     /**
      * Defines constants used by the plugin.
      *
@@ -84,20 +86,20 @@ class WordPress_Crm_Load
      */
     function constants()
     {
-        
+
         /* Set constant path to the members plugin directory. */
         define('CRM_DIR', trailingslashit(plugin_dir_path(__FILE__)));
-        
+
         /* Set constant path to the members plugin URL. */
         define('CRM_URI', trailingslashit(plugin_dir_url(__FILE__)));
-        
+
         /* Set the constant path to the members includes directory. */
         define('CRM_INCLUDES', CRM_DIR . trailingslashit('includes'));
-        
+
         /* Set the constant path to the members admin directory. */
         define('CRM_ADMIN', CRM_DIR . trailingslashit('admin'));
     }
-    
+
     /**
      * Loads the initial files needed by the plugin.
      *
@@ -105,14 +107,14 @@ class WordPress_Crm_Load
      */
     function includes()
     {
-        
+
         /* Load the plugin functions file. */
         require_once (CRM_INCLUDES . 'class-crm-list-table.php');
         require_once (CRM_INCLUDES . 'class-crm-users-list-table.php');
         require_once (CRM_INCLUDES . 'class-crm-car.php');
         require_once (CRM_INCLUDES . 'class-crm-customer.php');
     }
-    
+
     /**
      * Loads the translation files.
      *
@@ -120,11 +122,11 @@ class WordPress_Crm_Load
      */
     function i18n()
     {
-        
+
         /* Load the translation of the plugin. */
         load_plugin_textdomain('crm', false, 'crm/languages');
     }
-    
+
     /**
      * Loads the admin functions and files.
      *
@@ -132,15 +134,15 @@ class WordPress_Crm_Load
      */
     function admin()
     {
-        
+
         /* Only load files if in the WordPress admin. */
         if (is_admin()) {
-            
+
             /* Load the main admin file. */
             require_once (CRM_ADMIN . 'admin.php');
         }
     }
-    
+
     /**
      * Method that runs only when the plugin is activated.
      *
@@ -148,54 +150,45 @@ class WordPress_Crm_Load
      */
     function activation()
     {
-        $staff_role = add_role('department_staff', 'Department Staff');
-        $staff_caps = array(
-                'read_customers',
-                'edit_customers'
+        $staff_caps = array (
+            'read_customers' => 'read_customers',
+            'edit_customers' => 'edit_customers'
         );
-        foreach ($staff_caps as $cap) {
-            $department_role->add_cap($cap);
-        }
-        
-        $head_caps = array(
-                'read_customers',
-                'read_others_customers',
-                'edit_customers',
-                'edit_others_customers'
+        $staff_role = add_role('department_staff', 'Department Staff', $staff_caps);
+
+        $head_caps = array (
+            'read_customers' => 'read_customers',
+            'read_others_customers' => 'read_others_customers',
+            'edit_customers' => 'edit_customers',
+            'edit_others_customers' => 'edit_others_customers'
         );
-        $head_role = add_role('head_of_department', 'Head of Department');
-        foreach ($head_caps as $cap) {
-            $head_role->add_cap($cap);
-        }
-        
-        $manager_caps = array(
-                'read_customers',
-                'read_others_customers',
-                'edit_customers',
-                'edit_others_customers',
-                'create_users',
-                'edit_users',
-                'list_users',
-                'add_users',
-                'remove_users',
-                'promote_users'
+        $head_role = add_role('head_of_department', 'Head of Department', $head_caps);
+
+        $manager_caps = array (
+            'read_customers' => 'read_customers',
+            'read_others_customers' => 'read_others_customers',
+            'edit_customers' => 'edit_customers',
+            'edit_others_customers' => 'edit_others_customers',
+            'create_users' => 'create_users',
+            'edit_users' => 'edit_users',
+            'list_users' => 'list_users',
+            'add_users' => 'add_users',
+            'remove_users' => 'remove_users',
+            'promote_users' => 'promote_users'
         );
-        $manager_role = add_role('management_board', 'Management Board');
-        foreach ($manager_caps as $cap) {
-            $manager_role = $head_role->add_cap($cap);
-        }
+        $manager_role = add_role('management_board', 'Management Board', $manager_caps);
     }
-    
+
     /**
      * Method that runs only when the plugin is uninstalled.
      *
      * @since 0.2.0
      */
-    function uninstall()
+    function deactivation()
     {
-        remove_role('department_staff', 'Department Staff');
-        remove_role('head_of_department', 'Head of Department');
-        remove_role('management_board', 'Management Board');
+        remove_role('department_staff');
+        remove_role('head_of_department');
+        remove_role('management_board');
     }
 }
 
--- wp-content/plugins/crm/includes/class-crm-customer.php
+++ wp-content/plugins/crm/includes/class-crm-customer.php
@@ -1,1 +1,112 @@
 <?php
+class CRM_Customer {
+	public static function definition() {
+		return array (
+				'customer_information' => array (
+						'validation' => 'required',
+						'type' => 'group'
+				),
+				'full_name' => array (
+						'type' => 'field',
+						'validation' => 'required',
+						'group' => 'customer_information'
+				),
+				'address' => array (
+						'type' => 'field',
+						'group' => 'customer_information',
+						'validation' => 'required'
+				),
+				'phone' => array (
+						'type' => 'field',
+						'group' => 'customer_information',
+						'validation' => 'unique'
+				),
+				'current_cars' => array (
+						'type' => 'field'
+				),
+				'interested_cars' => array (
+						'type' => 'group'
+				),
+				'cars_model' => array (
+						'group' => 'interested_cars',
+						'type' => 'field',
+						'validation' => 'valid|car_models'
+				),
+				'cars_color' => array (
+						'group' => 'interested_cars',
+						'type' => 'field',
+						'validation' => 'valid|car_colors'
+				),
+				'others_notes' => array (
+						'group' => 'interested_cars',
+						'type' => 'field'
+				),
+				'customer_classification' => array (
+						'type' => 'group',
+						'validation' => 'required'
+				),
+				'hot_customer' => array (
+						'group' => 'customer_classification',
+						'type' => 'group'
+				),
+				'customer_use_cash' => array (
+						'type' => 'field',
+						'group' => 'customer_classification'
+				),
+				'customer_use_bank' => array (
+						'type' => 'group'
+				),
+				'loan_level' => array (
+						'type' => 'field',
+						'group' => 'customer_use_bank'
+				),
+				'bank_name' => array (
+						'type' => 'field',
+						'group' => 'customer_use_bank'
+				),
+				'need_consultant' => array (
+						'type' => 'field',
+						'group' => 'customer_use_bank'
+				),
+				'warm_customer' => array (
+						'group' => 'customer_classification',
+						'type' => 'field'
+				),
+				'cold_customer' => array (
+						'group' => 'customer_classification',
+						'type' => 'field'
+				),
+				'contract_signed' => array (
+						'type' => 'field'
+				),
+				'customer_source' => array (
+						'type' => 'group'
+				),
+				'showrom' => array (
+						'group' => 'customer_source',
+						'type' => 'field'
+				),
+				'sales_relations' => array (
+						'group' => 'customer_source',
+						'type' => 'field'
+				),
+				'mgmt_boards_relations' => array (
+						'group' => 'customer_source',
+						'type' => 'field'
+				),
+				'agency' => array (
+						'group' => 'customer_source',
+						'type' => 'field'
+				),
+				'others' => array (
+						'group' => 'customer_source',
+						'type' => 'field'
+				),
+				'notes' => array (
+						'type' => 'field'
+				),
+		);
+
+
+	}
+}
